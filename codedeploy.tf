e3c6cd0065b24cdf58846b4784f5b9e555532ea5
monsad <mon.sadlok@gmail.com>
2022-07-26 23:29:07 +0200

    name modification

--- codedeploy.tf
+++ codedeploy.tf
@@ -2,9 +2,9 @@ resource "aws_codedeploy_app" "ms-codedeploy" {
   name = "httpd-server"
 }
 
-resource "aws_codedeploy_deployment_group" "main" {
+resource "aws_codedeploy_deployment_group" "dg-ms" {
   app_name              = "${aws_codedeploy_app.ms-codedeploy.name}"
-  deployment_group_name = "Sample_DepGroup"
+  deployment_group_name = "DepGroupMS"
   service_role_arn      = "${aws_iam_role.codedeploy_service_ms.arn}"
 
   deployment_config_name = "CodeDeployDefault.OneAtATime"
